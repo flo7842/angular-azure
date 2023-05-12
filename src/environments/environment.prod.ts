31fd3edf3b57cfebe4fa93faa35d25b4f26acb84
flo7842 <florianbracq42@gmail.com>
2023-05-12 20:13:37 +0200

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    api_host: "",
+    api_host: "https://django-rest-api-todo.azurewebsites.net",
     azure: {
       connectionString: ""
     } 
